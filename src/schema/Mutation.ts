5aa3c0c6a1be8c15855c645e212da9671ee55f89
Michael Macias <zaeleus@gmail.com>
2017-07-25 20:53:10 -0500

    Add ArtistNameInput typedef

--- src/schema/Mutation.ts
+++ src/schema/Mutation.ts
@@ -1,14 +1,32 @@
 import Artist, { ArtistKind } from "../models/Artist";
 
+interface IAristNameInput {
+    id: string;
+    name: string;
+    locale: string;
+    isDefault: boolean;
+    isOriginal: boolean;
+}
+
 interface IArtistInput {
     id: string;
     kind: string;
+    names: IAristNameInput[];
 }
 
 export const typeDefs = `
+    input ArtistNameInput {
+        id: ID!
+        name: String!
+        locale: String!
+        isDefault: Boolean!
+        isOriginal: Boolean!
+    }
+
     input ArtistInput {
         id: ID!
         kind: String!
+        names: [ArtistNameInput]
     }
 
     type Mutation {
