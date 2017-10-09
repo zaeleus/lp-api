6a64096e6be082a51b330f12e82d14461152511a
Michael Macias <zaeleus@gmail.com>
2017-10-09 03:28:04 -0500

    Add basic createArtist mutation

--- src/schema/Mutation.ts
+++ src/schema/Mutation.ts
@@ -1,4 +1,5 @@
 import Artist, { ArtistKind } from "../models/Artist";
+import ArtistName from "../models/ArtistName";
 
 interface IAristNameInput {
     id: string;
@@ -11,6 +12,9 @@ interface IAristNameInput {
 interface IArtistInput {
     id: string;
     kind: string;
+    country: string;
+    startedOn?: string;
+    endedOn?: string;
     names: IAristNameInput[];
 }
 
@@ -26,16 +30,45 @@ export const typeDefs = `
     input ArtistInput {
         id: ID!
         kind: String!
+        country: String!
+        startedOn: String
+        endedOn: String
         names: [ArtistNameInput]
     }
 
     type Mutation {
+        createArtist(input: ArtistInput!): Artist!
         patchArtist(input: ArtistInput!): Artist!
     }
 `;
 
 export const resolvers = {
     Mutation: {
+        async createArtist(_root: any, { input }: { input: IArtistInput }): Promise<Artist> {
+            let artist;
+
+            try {
+                artist = await Artist.query().insert({
+                    // kind: input.kind,
+                    country: input.country,
+                });
+            } catch (err) {
+                throw new Error(err.message);
+            }
+
+            for (const name of input.names) {
+                await ArtistName.query().insert({
+                    // artist_id: artist.id,
+                    is_default: name.isDefault,
+                    is_original: name.isOriginal,
+                    locale: name.locale,
+                    name: name.name,
+                });
+            }
+
+            return artist;
+        },
+
         async patchArtist(_root: any, { input }: { input: IArtistInput }): Promise<Artist> {
             try {
                 return await Artist.query().patchAndFetchById(input.id, {
