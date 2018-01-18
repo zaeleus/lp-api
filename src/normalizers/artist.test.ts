32bb70d2540edffac24167e8fd1a472b2e6048fa
Michael Macias <zaeleus@gmail.com>
2018-01-18 16:02:59 -0600

    Reorganize artist normalizer/validator tests

--- src/normalizers/artist.test.ts
+++ src/normalizers/artist.test.ts
@@ -1,48 +1,50 @@
 import { ArtistKind } from "../models/Artist";
 import { rules } from "./artist";
 
-test("normalizeCountry", () => {
-    expect(rules.country("US")).toBe("US");
-    expect(rules.country("  US ")).toBe("US");
-    expect(rules.country(undefined)).toBe("");
-});
+describe("rules", () => {
+    test("country", () => {
+        expect(rules.country("US")).toBe("US");
+        expect(rules.country("  US ")).toBe("US");
+        expect(rules.country(undefined)).toBe("");
+    });
 
-test("normalizeDisambiguation", () => {
-    expect(rules.disambiguation("singer")).toBe("singer");
-    expect(rules.disambiguation("  singer ")).toBe("singer");
-    expect(rules.disambiguation("")).toBeUndefined();
-    expect(rules.disambiguation(undefined)).toBeUndefined();
-});
+    test("disambiguation", () => {
+        expect(rules.disambiguation("singer")).toBe("singer");
+        expect(rules.disambiguation("  singer ")).toBe("singer");
+        expect(rules.disambiguation("")).toBeUndefined();
+        expect(rules.disambiguation(undefined)).toBeUndefined();
+    });
 
-test("normalizeEndedOn", () => {
-    let d;
+    test("endedOn", () => {
+        let d;
 
-    d = rules.endedOn("2017-12");
-    expect(d.toString()).toBe("2017-12");
+        d = rules.endedOn("2017-12");
+        expect(d.toString()).toBe("2017-12");
 
-    d = rules.endedOn("");
-    expect(d.toString()).toBe("");
+        d = rules.endedOn("");
+        expect(d.toString()).toBe("");
 
-    d = rules.endedOn(undefined);
-    expect(d.toString()).toBe("");
-});
+        d = rules.endedOn(undefined);
+        expect(d.toString()).toBe("");
+    });
 
-test("normalizeKind", () => {
-    expect(rules.kind("PERSON")).toBe(ArtistKind.Person);
-    expect(rules.kind("GROUP")).toBe(ArtistKind.Group);
-    expect(rules.kind("foo")).toBe(ArtistKind.Unknown);
-    expect(rules.kind(undefined)).toBe(ArtistKind.Unknown);
-});
+    test("kind", () => {
+        expect(rules.kind("PERSON")).toBe(ArtistKind.Person);
+        expect(rules.kind("GROUP")).toBe(ArtistKind.Group);
+        expect(rules.kind("foo")).toBe(ArtistKind.Unknown);
+        expect(rules.kind(undefined)).toBe(ArtistKind.Unknown);
+    });
 
-test("normalizeStartedOn", () => {
-    let d;
+    test("startedOn", () => {
+        let d;
 
-    d = rules.startedOn("2017-12");
-    expect(d.toString()).toBe("2017-12");
+        d = rules.startedOn("2017-12");
+        expect(d.toString()).toBe("2017-12");
 
-    d = rules.startedOn("");
-    expect(d.toString()).toBe("");
+        d = rules.startedOn("");
+        expect(d.toString()).toBe("");
 
-    d = rules.startedOn(undefined);
-    expect(d.toString()).toBe("");
+        d = rules.startedOn(undefined);
+        expect(d.toString()).toBe("");
+    });
 });
--- src/validators/artist.test.ts
+++ src/validators/artist.test.ts
@@ -1,58 +1,60 @@
 import { ArtistKind } from "../models/Artist";
 import validate, { rules } from "./artist";
 
-test("country", () => {
-    expect(rules.country("US")).toBe(true);
-    expect(rules.country("XX")).toBe(true);
+describe("rules", () => {
+    test("country", () => {
+        expect(rules.country("US")).toBe(true);
+        expect(rules.country("XX")).toBe(true);
 
-    expect(() => rules.country("")).toThrow();
-    expect(() => rules.country("U")).toThrow();
-    expect(() => rules.country("USA")).toThrow();
-    expect(() => rules.country("United States")).toThrow();
-});
+        expect(() => rules.country("")).toThrow();
+        expect(() => rules.country("U")).toThrow();
+        expect(() => rules.country("USA")).toThrow();
+        expect(() => rules.country("United States")).toThrow();
+    });
 
-test("kind", () => {
-    expect(rules.kind(ArtistKind.Person)).toBe(true);
-    expect(rules.kind(ArtistKind.Group)).toBe(true);
+    test("kind", () => {
+        expect(rules.kind(ArtistKind.Person)).toBe(true);
+        expect(rules.kind(ArtistKind.Group)).toBe(true);
 
-    expect(() => rules.kind(ArtistKind.Unknown)).toThrow();
-});
+        expect(() => rules.kind(ArtistKind.Unknown)).toThrow();
+    });
 
-test("names", () => {
-    expect(() => rules.names([])).toThrow();
+    test("names", () => {
+        expect(() => rules.names([])).toThrow();
 
-    expect(rules.names([
-        { name: "a", locale: "en", isOriginal: true, isDefault: false },
-        { name: "b", locale: "en", isOriginal: false, isDefault: true },
-    ])).toBe(true);
-
-    expect(rules.names([
-        { name: "a", locale: "en", isOriginal: true, isDefault: true },
-        { name: "b", locale: "en", isOriginal: false, isDefault: false },
-    ])).toBe(true);
-
-    expect(() => {
-        rules.names([
-            { name: "a", locale: "en", isOriginal: true, isDefault: true },
+        expect(rules.names([
+            { name: "a", locale: "en", isOriginal: true, isDefault: false },
             { name: "b", locale: "en", isOriginal: false, isDefault: true },
-        ]);
-    }).toThrow();
+        ])).toBe(true);
 
-    expect(() => {
-        rules.names([
+        expect(rules.names([
             { name: "a", locale: "en", isOriginal: true, isDefault: true },
-            { name: "b", locale: "en", isOriginal: true, isDefault: true },
-        ]);
-    }).toThrow();
-});
+            { name: "b", locale: "en", isOriginal: false, isDefault: false },
+        ])).toBe(true);
 
-test("validate", () => {
-    expect(validate({})).toBe(true);
-    expect(validate({ country: "US" })).toBe(true);
-    expect(validate({
-        country: "US",
-        kind: ArtistKind.Person,
-    })).toBe(true);
+        expect(() => {
+            rules.names([
+                { name: "a", locale: "en", isOriginal: true, isDefault: true },
+                { name: "b", locale: "en", isOriginal: false, isDefault: true },
+            ]);
+        }).toThrow();
 
-    expect(() => validate({ country: "" })).toThrow();
+        expect(() => {
+            rules.names([
+                { name: "a", locale: "en", isOriginal: true, isDefault: true },
+                { name: "b", locale: "en", isOriginal: true, isDefault: true },
+            ]);
+        }).toThrow();
+    });
+
+    test("validate", () => {
+        expect(validate({})).toBe(true);
+        expect(validate({ country: "US" })).toBe(true);
+        expect(validate({
+            country: "US",
+            kind: ArtistKind.Person,
+        })).toBe(true);
+
+        expect(() => validate({ country: "" })).toThrow();
+    });
 });
