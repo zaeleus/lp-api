92539fd90a6470bcadda5bc966254f7558b24666
Michael Macias <zaeleus@gmail.com>
2018-03-12 03:34:46 -0500

    mutation: Add createArtist tests

---
+++ src/schema/mutation/Mutation.test.ts
@@ -0,0 +1,88 @@
+import Artist, { ArtistKind } from "../../models/Artist";
+import { resolvers } from "./Mutation";
+
+const DEFAULT_PAYLOAD = {
+    input: {
+        country: "KR",
+        disambiguation: "",
+        endedOn: "",
+        kind: "PERSON",
+        startedOn: "1993-08-12",
+
+        names: [{
+            isDefault: false,
+            isOriginal: true,
+            locale: "ko",
+            name: "루나",
+        }, {
+            isDefault: true,
+            isOriginal: false,
+            locale: "ko-Latn",
+            name: "Luna",
+        }],
+    },
+};
+
+describe("mutations Root resolvers", () => {
+   describe("createArtist", () => {
+        describe("with valid input", () => {
+            let artist: Artist;
+
+            beforeAll(async () => {
+                artist = await resolvers.Mutation.createArtist({}, DEFAULT_PAYLOAD);
+                artist = await artist.$loadRelated("names(orderById)", {
+                    orderById: (builder) => builder.orderBy("id"),
+                });
+            });
+
+            test("creates a new artist", () => {
+                expect(artist.id).not.toBe(null);
+                expect(artist.country).toBe("KR");
+                expect(artist.disambiguation).toBe(null);
+                expect(artist.ended_on_year).toBe(null);
+                expect(artist.ended_on_month).toBe(null);
+                expect(artist.ended_on_day).toBe(null);
+                expect(artist.kind).toBe(ArtistKind.Person);
+                expect(artist.started_on_year).toBe(1993);
+                expect(artist.started_on_month).toBe(8);
+                expect(artist.started_on_day).toBe(12);
+            });
+
+            test("creates artist names", () => {
+                expect.assertions(9);
+
+                const names = artist.names;
+                if (!names) { return; }
+
+                expect(names.length).toBe(2);
+
+                expect(names[0].is_default).toBe(false);
+                expect(names[0].is_original).toBe(true);
+                expect(names[0].locale).toBe("ko");
+                expect(names[0].name).toBe("루나");
+
+                expect(names[1].is_default).toBe(true);
+                expect(names[1].is_original).toBe(false);
+                expect(names[1].locale).toBe("ko-Latn");
+                expect(names[1].name).toBe("Luna");
+            });
+        });
+
+        describe("with invalid input", () => {
+            const createArtist = () => {
+                return resolvers.Mutation.createArtist({}, {
+                    input: {
+                        country: "",
+                        kind: "",
+                        names: [],
+                    },
+                });
+            };
+
+            test("throws an error", async () => {
+                expect.assertions(1);
+                await expect(createArtist()).rejects.toThrow();
+            });
+        });
+    });
+});
